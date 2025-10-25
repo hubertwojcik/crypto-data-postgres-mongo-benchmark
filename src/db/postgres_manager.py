4f24551791e515cc65bd524343bd3fda8562d690
Hubert Wójcik <hubertwojcik@MacBook-Pro-Hubert.local>
2025-10-25 11:44:56 +0200

    Add DDL

--- src/db/postgres_manager.py
+++ src/db/postgres_manager.py
@@ -2,7 +2,45 @@ import psycopg2
 from psycopg2.extras import execture_batch
 
 DDL = """
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  user_name TEXT UNIQUE,
+  user_location TEXT,
+  user_description TEXT,
+  user_created TIMESTAMP,
+  user_followers BIGINT,
+  user_friends BIGINT,
+  user_favourites BIGINT,
+  user_verified BOOLEAN
+);
 
+CREATE TABLE IF NOT EXISTS sources (
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS tweets (
+  id BIGSERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  date TIMESTAMP,
+  text TEXT,
+  source_id INT REFERENCES sources(id),
+  is_retweet BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS hashtags (
+  id SERIAL PRIMARY KEY,
+  tag TEXT UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS tweet_hashtags (
+  tweet_id BIGINT REFERENCES tweets(id) ON DELETE CASCADE,
+  hashtag_id INT REFERENCES hashtags(id) ON DELETE CASCADE,
+  PRIMARY KEY (tweet_id, hashtag_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_tweets_user_date ON tweets(user_id, date);
+CREATE INDEX IF NOT EXISTS idx_hashtag_tag ON hashtags(tag);
 """
 
 class PostgresManager:
